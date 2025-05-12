ca9c13cdeaff912432042246a5b2c4eb266dae6b
Steven <steven@nenyah.com>
2025-05-13 07:56:35 +0800

    Add MessageHelper class for handling success and error messages

---
+++ com.amtec.forms/MessageHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+namespace SendPartno
+{
+    class MessageHelper
+    {
+        private Form1 _form1;
+        public MessageHelper(Form1 form1)
+        {
+            _form1 = form1;
+        }
+        private void SuccessMSG(string message)
+        {
+            _form1.label1.Text = message;
+            _form1.label1.ForeColor = Color.Blue;
+        }
+        private void ErrorMSG(string message)
+        {
+            _form1.label1.Text = message;
+            _form1.label1.ForeColor = Color.Red;
+        }
+    }
+}
